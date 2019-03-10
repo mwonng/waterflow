43ba70435f82e1a18c8db6ebeb3e5ce723fd136d
Michael Wonng <michael@wonng.com>
2019-03-10 17:35:45 +1100

    simulate all cups with water

--- func/index.js
+++ func/index.js
@@ -12,8 +12,22 @@ var func = {
     return cupArray;
   },
 
-  pourWater: function (amount, cupArray, glassCap) {
+  pourWater: function (amount, cupArray, glassCap, row, col) {
+    let rows = cupArray.length;
+    cupArray[0][0] = amount;
 
+    for (i = 0; i < rows ; i++) {
+      for (j = 0; j <= i ; j++) {
+
+        if (cupArray[i][j] > glassCap) {
+          cupArray[i+1][j] += (cupArray[i][j] - glassCap ) / 2;
+          cupArray[i+1][j+1] += (cupArray[i][j] - glassCap ) / 2;
+          cupArray[i][j] = glassCap;
+        }
+      }
+    }
+
+    return cupArray;
   }
 }
 
