51853a823e0b1ae85aa000b1c2071c147996272f
Michael Wonng <michael@wonng.com>
2019-03-10 17:41:10 +1100

    add test case for pouring water

--- test/index.js
+++ test/index.js
@@ -12,15 +12,13 @@ module.exports = testEqual
 
 
 
-console.log(testEqual(func.generateCups(5).length, 5));
-
 // show all cups
-console.log(func.pourWater(1000, func.generateCups(5), 250, 0, 0));
+console.log("show all cups", func.pourWater(1000, func.generateCups(5), 250, 0, 0));
 
-// console.log(testEqual(func.pourWater(1000, func.generateCups(5), 250, 0, 0), 250));
+console.log("Pour water opion 1 ->", testEqual(func.pourWater(1000, func.generateCups(5), 250, 0, 0), 250));
 
-// console.log(testEqual(func.pourWater(1000, func.generateCups(5), 250, 1, 1), 250));
+console.log("Pour water opion 2 ->", testEqual(func.pourWater(1000, func.generateCups(5), 250, 1, 1), 250));
 
-// console.log(testEqual(func.pourWater(1000, func.generateCups(5), 250, 3, 0), 0));
+console.log("Pour water opion 3 ->", testEqual(func.pourWater(1000, func.generateCups(5), 250, 3, 0), 0));
 
-// console.log(testEqual(func.pourWater(1200, func.generateCups(5), 250, 0, 0), 250));
+console.log("Pour water opion 4 ->", testEqual(func.pourWater(2200, func.generateCups(5), 250, 2, 0), 250));
