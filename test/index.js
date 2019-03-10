a9c98b4d8029455629979f784db6f34e20f5a586
Michael Wonng <michael@wonng.com>
2019-03-10 17:28:05 +1100

    test simulate all cups

--- test/index.js
+++ test/index.js
@@ -10,4 +10,17 @@ module.exports = testEqual
 
 
 
+
+
 console.log(testEqual(func.generateCups(5).length, 5));
+
+// show all cups
+console.log(func.pourWater(1000, func.generateCups(5), 250, 0, 0));
+
+// console.log(testEqual(func.pourWater(1000, func.generateCups(5), 250, 0, 0), 250));
+
+// console.log(testEqual(func.pourWater(1000, func.generateCups(5), 250, 1, 1), 250));
+
+// console.log(testEqual(func.pourWater(1000, func.generateCups(5), 250, 3, 0), 0));
+
+// console.log(testEqual(func.pourWater(1200, func.generateCups(5), 250, 0, 0), 250));
